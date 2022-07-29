84219e3aa5c0e15c3b96bc28bdeae9e53b725a92
Alisson <alisson.silveira@kbase.com.br>
2022-07-29 19:00:17 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import buildings from './images/buildings.svg';
 import './App.css';
+import bricks from './images/bricks.svg';
+import excavator from './images/excavator1.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='mainDiv'>
+      <header style={{ backgroundImage: `url(${buildings})`, backgroundSize: "100%", height: "185px" }}>
+
       </header>
+      <div className='middlePage'>
+        <div>
+          <h1 className='mainTitles'>BOTA UMA FRASE <br></br>
+            ENGRAÇADA AQUI</h1>
+        </div>
+        <div className='mainText'>
+          Lornyhcpole eu te amoooooem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Sed ut perspiciatis eu posso facilmente assassinar um leão da montanha doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
+        </div>
+        <div>
+          <button className='mainButton' style={{ backgroundImage: `url(${bricks})`, backgroundSize: "25%" }}>
+            CLICA AQUI <br></br>
+            RAPIDÃO
+          </button>
+        </div>
+      </div>
+
+      <footer style={{ backgroundImage: `url(${excavator})`, backgroundSize: "100%", height: "264px" }}>
+
+      </footer>
+
     </div>
   );
 }
